5a469218f10fd1f43640895cf4cfe980c69463d1
Paweł Żydziak <pawel.zydziak@relayr.io>
2020-05-08 18:27:34 +0200

    Initial commit

---
+++ bot.py
@@ -0,0 +1,25 @@
+import facebook
+import datetime
+import json
+
+week_days = ["poniedziałek", "wtorek", "środa", "czwartek", "piątek"]
+with open('data.json') as json_file:
+  data = json.load(json_file)
+
+access_page = data["access_page"]
+user_id = data["user_id"]
+app_secret = data["app_secret"]
+page_id = data["page_id"]
+
+client = facebook.GraphAPI(access_token=access_page, version="3.0")
+
+current_week_day = datetime.datetime.today().weekday()
+# print(week_days[current_week_day])
+
+how_many_days = datetime.date.today() - datetime.date(2020, 3, 12)
+# print(how_many_days.days)
+
+msg = "[PYTHON BOT TEST] " + "#" + str(how_many_days.days) + ":" + " Dziś jest " + str(week_days[current_week_day]) + "."
+print(msg)
+
+client.put_object(parent_object=page_id, connection_name="feed", message=msg)
