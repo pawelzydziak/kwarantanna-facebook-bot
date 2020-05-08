34d94ab30b523e5eeed88493bc95b1b9b12979cc
Paweł Żydziak <pawel.zydziak@relayr.io>
2020-05-08 19:37:48 +0200

    cron related fixes

--- bot.py
+++ bot.py
@@ -1,12 +1,14 @@
+import requests
 import facebook
 import datetime
 import json
 
 week_days = ["poniedziałek", "wtorek", "środa", "czwartek", "piątek"]
-with open('data.json') as json_file:
+with open('/home/pzydziak/PycharmProjects/kwarantannaBot/data.json') as json_file:
   data = json.load(json_file)
 
 access_page = data["access_page"]
+access_user = data["access_user"]
 user_id = data["user_id"]
 app_secret = data["app_secret"]
 page_id = data["page_id"]
@@ -19,7 +21,7 @@ current_week_day = datetime.datetime.today().weekday()
 how_many_days = datetime.date.today() - datetime.date(2020, 3, 13)
 # print(how_many_days.days)
 
-msg = "[PYTHON BOT TEST] " + "#" + str(how_many_days.days) + ":" + " Dziś jest " + str(week_days[current_week_day]) + "."
-print(msg)
+msg = "[CRON PYTHON BOT TEST] " + "#" + str(how_many_days.days) + ":" + " Dziś jest " + str(week_days[current_week_day]) + "."
+# print(msg)
 
 client.put_object(parent_object=page_id, connection_name="feed", message=msg)
